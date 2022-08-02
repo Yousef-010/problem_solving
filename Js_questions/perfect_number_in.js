f9342d0558bc35f0a4abb73f619cb42df751e13f
Yousef Al Farwan <yousefsyria304@gmail.com>
2022-08-02 21:21:37 +0300

    update: perfect_number_between func

--- Js_questions/perfect_number_in.js
+++ Js_questions/perfect_number_in.js
@@ -1,7 +1,7 @@
 function perfectly(num){
     let temp = 0 
 
-    for (let i=1; i < num; i++) {
+    for (let i=1; i <= num/2; i++) {
         if (num % i == 0) {
             temp += i 
         }}
